e883ba67e7eea10c81d6c5b64f6d35c232f211fe
INnoVatioN-97 <koyoungil97@gmail.com>
2022-02-13 18:06:06 +0900

    README 수정

--- README.md
+++ README.md
@@ -117,11 +117,14 @@ export default store;
 #### 변경
 
 ```javascript
+// 액션명을 정의
 const addToDo = createAction('ADD');
 const deleteToDo = createAction('DELETE');
 
 /**
- * createReducer (초기상태값, 분기)
+ * createReducer (초기상태값, 분기){
+ *  [액션명] : (state, action) => {세부 동작}
+ * }
  * = 새 state를 return 하거나, mutate할 수 있다.
  */
 const reducer = createReducer([], {
@@ -129,14 +132,13 @@ const reducer = createReducer([], {
 		state.push({ text: action.payload, id: Date.now() });
 	},
 	[deleteToDo]: (state, action) => {
-		// console.log(action.payload);
-		// console.log(state);
 		return state.filter((toDo) => toDo.id !== action.payload);
 	},
 });
 
 const store = createStore(reducer);
 
+// 이전과 동일
 export const actionCreators = {
 	addToDo,
 	deleteToDo,
--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { createStore } from 'redux';
 import { createAction, createReducer } from '@reduxjs/toolkit';
 
+// 액션명을 정의
 const addToDo = createAction('ADD');
 const deleteToDo = createAction('DELETE');
 
 /**
- * createReducer (초기상태값, 분기)
+ * createReducer (초기상태값, 분기){
+ *  [액션명] : (state, action) => {세부 동작}
+ * }
  * = 새 state를 return 하거나, mutate할 수 있다.
  */
 const reducer = createReducer([], {
@@ -13,14 +16,13 @@ const reducer = createReducer([], {
 		state.push({ text: action.payload, id: Date.now() });
 	},
 	[deleteToDo]: (state, action) => {
-		// console.log(action.payload);
-		// console.log(state);
 		return state.filter((toDo) => toDo.id !== action.payload);
 	},
 });
 
 const store = createStore(reducer);
 
+// 이전과 동일
 export const actionCreators = {
 	addToDo,
 	deleteToDo,
